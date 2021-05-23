f0dbaf8df3360d40702d103c17c4ff4e85a1077c
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-05-23 15:29:38 +0200

    Add error message for api

--- js/coindesk.js
+++ js/coindesk.js
@@ -2,12 +2,19 @@ var coindeskapp = new Vue({
     el: "#coindesk",
     data: {
         info: null,
+        loading: true,
+        hasError: false,
     },
     created:
         function(){
             axios
                 .get('https://api.coindesk.com/v1/bpi/currentprice.json')
                 .then(response => (this.info = response.data.bpi))
+                .catch(error => {
+                    console.log(error)
+                    this.hasError = true
+                })
+                .finally(() => this.loading = false)
         },
     
 });
