ccc021e02730cd6e5650401a3216ac806d06e6ce
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-05-23 12:52:12 +0200

    Update index and app.js on loops

--- js/app.js
+++ js/app.js
@@ -4,5 +4,10 @@ var app = new Vue({
         pageTitle: "Hello Vue from Aartrijke",
         hoverText: "Page loaded on " + new Date().toLocaleDateString(),
         canYouSeeMe: true,
+        people: [
+            {name: "Xander", city: "Aalst"},
+            {name: "William", city: "Brugge"},
+            {name: "Els", city: "Roeselare"},
+        ],
     },
 })
