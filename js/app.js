975ece15ead22a01fd680e1d97bdbf8b2faeddab
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-05-23 12:35:39 +0200

    Add start to learn Vue

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        pageTitle: "Hello Vue from Aartrijke",
+        hoverText: "Page loaded on " + new Date().toLocaleDateString(),
+    },
+})
